6a08d479401806b06e11f65405f31e78d2aeabef
Solodov <solodov.dev@gmail.com>
2020-01-15 15:24:04 +0300

    Folder structure

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import createButton from "./src/modules/Button.js";
+
 let cvs = document.getElementById("canvas");
 let ctx = cvs.getContext("2d");
 
@@ -6,43 +8,10 @@ let bg = new Image();
 bg.src = "guitar.png";
 let keys = ["KeyH", "KeyJ", "KeyK", "KeyL"];
 
-// Coordinates
-let btnX = 155;
-let btnY = 420;
-let offset = 2;
-
-function createButton(index) {
-  let image = new Image();
-  image.src = "button.png";
-  let x = btnX + index * (offset + image.width);
-  let y = btnY;
-  let isPressed = false;
-  return {
-    image,
-    isPressed,
-    x,
-    y
-  };
-}
-
-function createShadow(index) {
-  let image = new Image();
-  image.src = "shadow.png";
-  let x = btnX + index * (offset + image.width);
-  let y = btnY;
-  return {
-    image,
-    x,
-    y
-  };
-}
-
 let buttons = [];
-let shadows = [];
 
 for (let i = 0; i < keys.length; i++) {
   buttons.push(createButton(i));
-  shadows.push(createShadow(i));
 }
 
 document.addEventListener("keydown", function(event) {
